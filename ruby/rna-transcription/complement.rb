ed138720ce0fb2a2e6f089fd763c44342e966147
Kurniawan <contact@iwanwan.me>
2014-09-11 00:00:48 +0700

    make it more simple, move logic to one method

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,21 +1,19 @@
 class Complement
 
-  @dna_rna_complements = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+  @dna_complements = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
 
   def self.of_dna(rna_strands)
-    dna = '';
-    rna_strands.chars.each do | val |
-      dna += @dna_rna_complements[val]
-    end
-    return dna
+    self.complements_values(@dna_complements, rna_strands);
   end
 
   def self.of_rna(dna_strands)
-    rna = '';
-    dna_strands.chars.each do | val |
-      rna += @dna_rna_complements.key(val)
-    end
-    return rna
+    self.complements_values(@dna_complements.invert, dna_strands);
+  end
+
+  def self.complements_values(complements, keys)
+    values = '';
+    keys.chars.each{ | val | values += complements.fetch(val, '') }
+    return values
   end
 
 end
