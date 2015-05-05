4aa2da21a1ca72da5d1b37199a79ba0d6c991cf2
Kurniawan <contact@iwanwan.me>
2015-05-06 06:39:41 +0700

    finish raindrops

--- ruby/raindrops/raindrops_test.rb
+++ ruby/raindrops/raindrops_test.rb
@@ -1,15 +1,6 @@
 require 'minitest/autorun'
 require_relative 'raindrops'
 
-# irb(main):025:0> factors = Prime.prime_division(1755)
-# => [[3, 3], [5, 1], [13, 1]]
-# irb(main):026:0> s = {3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }
-# => {3=>"Pling", 5=>"Plang", 7=>"Plong"}
-# irb(main):027:0> s.values_at(3,5).each{ |a| puts a }
-# Pling
-# Plang
-# => ["Pling", "Plang"]
-
 class RaindropsTest < MiniTest::Unit::TestCase
 
   def test_1
@@ -17,77 +8,62 @@ class RaindropsTest < MiniTest::Unit::TestCase
   end
 
   def test_3
-    skip
     assert_equal "Pling", Raindrops.convert(3)
   end
 
   def test_5
-    skip
     assert_equal "Plang", Raindrops.convert(5)
   end
 
   def test_7
-    skip
     assert_equal "Plong", Raindrops.convert(7)
   end
 
   def test_6
-    skip
     assert_equal "Pling", Raindrops.convert(6)
   end
 
   def test_9
-    skip
     assert_equal "Pling", Raindrops.convert(9)
   end
 
   def test_10
-    skip
     assert_equal "Plang", Raindrops.convert(10)
   end
 
   def test_14
-    skip
     assert_equal "Plong", Raindrops.convert(14)
   end
 
   def test_15
-    skip
     assert_equal "PlingPlang", Raindrops.convert(15)
   end
 
   def test_21
-    skip
     assert_equal "PlingPlong", Raindrops.convert(21)
   end
 
   def test_25
-    skip
     assert_equal "Plang", Raindrops.convert(25)
   end
 
   def test_35
-    skip
     assert_equal "PlangPlong", Raindrops.convert(35)
   end
 
   def test_49
-    skip
     assert_equal "Plong", Raindrops.convert(49)
   end
 
   def test_52
-    skip
     assert_equal "52", Raindrops.convert(52)
   end
 
   def test_105
-    skip
     assert_equal "PlingPlangPlong", Raindrops.convert(105)
   end
 
   def test_12121
-    skip
     assert_equal "12121", Raindrops.convert(12121)
   end
 
