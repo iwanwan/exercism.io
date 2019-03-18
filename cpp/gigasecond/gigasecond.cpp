d4340b6f7be1243cdde09e4283267e2075d8f216
Kurniawan <contact@iwanwan.me>
2019-03-18 21:40:23 +0700

    move guard to beginning of file and simplify calculation

--- cpp/gigasecond/gigasecond.cpp
+++ cpp/gigasecond/gigasecond.cpp
@@ -1,12 +1,11 @@
 #include "gigasecond.h"
-#include "boost/date_time/posix_time/posix_time.hpp"
 
 using namespace boost::posix_time;
 
 namespace gigasecond
 {
-  boost::posix_time::ptime advance(boost::posix_time::ptime moment)
+  boost::posix_time::ptime advance(ptime moment)
   {
-    return time_period(moment, seconds(1000000000)).end();
+    return moment + seconds(1000000000);
   }
 }
--- cpp/gigasecond/gigasecond.h
+++ cpp/gigasecond/gigasecond.h
@@ -1,10 +1,11 @@
+#if !defined(GIGASECOND_H)
+#define GIGASECOND_H
 #include "boost/date_time/posix_time/posix_time.hpp"
 #define EXERCISM_RUN_ALL_TESTS
 
-#if !defined(GIGASECOND_H)
-#define GIGASECOND_H
 namespace gigasecond
 {
   boost::posix_time::ptime advance(boost::posix_time::ptime moment);
 }
+
 #endif
